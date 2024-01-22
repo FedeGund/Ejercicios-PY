72fa23cdf3221d3bb7b50c007796eebfd2dfdacf
Fede <fedegundlach@gmail.com>
2024-01-22 22:10:54 +0100

    Agrego el segundo ejercicio

---
+++ Cap2Ej4.py
@@ -0,0 +1,17 @@
+import msvcrt
+# Assume that we execute the following assignment statements:
+# width = 17
+# height = 12.0
+# For each of the following expressions, write the value of the expression and the
+# type (of the value of the expression).
+# 1. width//2 la doble barra hace una division entera
+# 2. width/2.0 la barra sola hace la division normal
+# 3. height/3 
+# 4. 1 + 2 * 5
+width = 17
+height = 12.0
+print('1. width//2 - ',width//2)
+print('2. width/2.0 - ',width/2.0)
+print('3. height/3 - ',height/3)
+print('4. 1 + 2 * 5 - ',1 + 2 * 5)
+msvcrt.getch()
